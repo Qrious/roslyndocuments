957063c29d143d77c8b8488148939a17e79dbd74
Sander Klock <SKL@afasgroep.nl>
2017-08-23 12:55:31 +0200

    Initial commit

---
+++ RoslynApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+
+namespace RoslynApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Task.Run(async () =>
+      {
+        var workspace = MSBuildWorkspace.Create();
+        var solution = await workspace.OpenSolutionAsync("../../../ConsoleApp1.sln");
+        var project = solution.Projects.Single(p => p.Name == "ConsoleApp1");
+        var hasDocuments = project.HasDocuments;
+        var documents = project.Documents;
+        Console.WriteLine($"Has documents: {hasDocuments}");
+        Console.WriteLine($"Documents count: {documents.Count()}");
+        Console.WriteLine("Filepath of documents (if any):");
+        foreach(var document in documents)
+        {
+          Console.WriteLine($"- {document.FilePath}");
+        }
+        Console.ReadLine();
+      }).GetAwaiter().GetResult();
+    }
+  }
+}
